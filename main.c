24e94ee7b50285916fdf5bcd0980d7153c993053
mae <maelys.guillot21@gmail.com>
2024-04-30 16:52:13 +0200

    edit main

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "Grille.h"
+#include <curses.h>
 
 int main()
 {
+    int n = 7;
+    int m = 4;
+    g * grille_test = Grille_allouer(n, m);
+    Grille_vider(grille_test);
 }
